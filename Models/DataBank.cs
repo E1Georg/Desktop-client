225980335a3296ae3645780f11a7a950d48f860f
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2022-01-29 15:39:37 +0300

    class

---
+++ Models/DataBank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms
+{
+    static class DataBank
+    {
+        public static int key = 0;
+        public static bool needDB = false;
+        public static int id_string = -1;
+    }
+}
---
+++ Models/UserException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Forms
+{
+    class UserException
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Message { get; set; }
+        public string TargetSite { get; set; }
+        public System.DateTime dateTimeExc { get; set; }
+        public int indexForm { get; set; }
+    }
+}

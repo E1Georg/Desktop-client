c82bf6eda14996cef0dbd50e83897b3aade6e319
Efim Shumkov <47829068+E1Georg@users.noreply.github.com>
2022-01-29 15:42:44 +0300

    Config

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Forms
+{
+    class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("DbConnection")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<UserException> UserExceptions { get; set; }
+    }
+}
